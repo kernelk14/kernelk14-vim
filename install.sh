b9413e0ed74275c39ed6a2cef4c91b9368c29103
kernelk14 <khyleisaias@gmail.com>
2023-05-05 08:36:05 +0800

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# This is a script for Vimrc
+FILE=/home/$(whoami)/.vimrc
+NEWFILE=/home/$(whoami)/.vimrc.bak
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists, backing up your old .vimrc file"
+    mv "$FILE" -v "$NEWFILE"
+    if [ -f "/usr/bin/wget" ]; then
+      wget https://raw.githubusercontent.com/kernelk14/kernelk14-vim/master/.vimrc
+    else
+      echo "wget is not installed!"
+    fi
+    if [ -f "/usr/bin/curl" ]; then
+      curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    else
+      echo "curl is not installed!"
+    fi
+else
+    echo "It seems that $FILE does not exist. Creating new config."
+    if [ -f "/usr/bin/wget" ]; then
+      wget https://raw.githubusercontent.com/kernelk14/kernelk14-vim/master/.vimrc
+    else
+      echo "wget is not installed!"
+    fi
+    if [ -f "/usr/bin/curl" ]; then
+      curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    else
+      echo "curl is not installed!"
+    fi
+fi
+
